060b470ab64027784ff7aaa93f1d98aea39c8b8b
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-23 23:56:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,11 @@ class Song
     @@all
   end
 
+  def artist_name(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+  end
+
 
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
