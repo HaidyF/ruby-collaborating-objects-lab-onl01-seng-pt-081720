bf1849e18db07c7229d20493ba9d1500f1ec1f77
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-23 23:41:39 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize (filepath)
+    @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    # @files.delete_if {|file| file == "." || file == ".."}
+    puts @files
+  end
+
+  def import
+    delf.files.each do |file|
+      song = song.new_by_filename(file)
+      Artist.all << song.artist unless Artist.all.include?(song.artist)
+    end
+  end
+end
